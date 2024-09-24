958b4d391cd5b9cc1a287a426b1f1fc785346803
unknown <christianmabeladi@gmail.com>
2024-09-24 13:08:03 +0100

    fix connect Api with signup

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
-import { useNavigation } from "@react-navigation/native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import * as Animatable from "react-native-animatable";
 import { Eye, EyeOff } from "react-native-feather";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+type RootStackParamList = {
+  index: undefined;
+  signup: undefined;
+};
+
 export default function LoginScreen() {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   return (
     <SafeAreaView className="flex-1 bg-teal-500">
@@ -21,17 +26,17 @@ export default function LoginScreen() {
         animation="fadeInUp"
         className="flex-3 bg-white rounded-t-3xl p-8"
       >
-        <Text className="text-gray-800 text-lg mt-5">Username</Text>
+        <Text className="text-gray-800 text-lg mt-5">Nom de l'utilisateur</Text>
         <TextInput
           className="border-b border-gray-200 mb-5 text-gray-800 text-lg"
-          placeholder="Enter your username"
+          // placeholder="Entrez votre nom d'utilisateur"
           placeholderTextColor="gray"
         />
-        <Text className="text-gray-800 text-lg mt-5">Password</Text>
+        <Text className="text-gray-800 text-lg mt-5">mot de passe</Text>
         <View className="flex-row items-center border-b border-gray-200 mb-5">
           <TextInput
             className="flex-1 text-gray-800 text-lg"
-            placeholder="Enter your password"
+            // placeholder="Entrez le mot de passe"
             placeholderTextColor="gray"
             secureTextEntry={!passwordVisible}
           />
@@ -46,14 +51,14 @@ export default function LoginScreen() {
           </TouchableOpacity>
         </View>
         <TouchableOpacity className="bg-teal-500 p-4 rounded-lg items-center mt-8">
-          <Text className="text-white text-lg font-bold">Login</Text>
+          <Text className="text-white text-lg font-bold">Se connecté</Text>
         </TouchableOpacity>
         <TouchableOpacity
           className="mt-4 items-center"
-          // onPress={() => navigation.navigate("SignupScreen")}
+          onPress={() => navigation.navigate("signup")}
         >
-          <Text className="text-teal-500 text-lg">
-            Don't have an account? Sign up
+          <Text className="text-teal-500 text-sm">
+            Vous n'avez pas de compte ? S'inscrire{" "}
           </Text>
         </TouchableOpacity>
       </Animatable.View>
