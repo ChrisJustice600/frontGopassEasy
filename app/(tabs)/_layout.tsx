b426b2fa167aad09a6ae914775ec01b9f97040ed
christian mabeladi <christianmabeladi@gmail;com>
2024-11-03 22:12:24 +0100

    fix status bar dark mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,44 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import React from "react";
+import { StatusBar } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Accueil",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-        }}
-      />
+    <>
+      {/* Configurer la barre d'état en sombre */}
+      <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
+      <Tabs>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Accueil",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="ticket"
-        options={{
-          title: "Ticket",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ticket" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profil",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="ticket"
+          options={{
+            title: "Ticket",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ticket" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profil",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
